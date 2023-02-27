a9999f5b416523edf099d09589ae9698dc9b5975
Javier Cáceres Bordallo <javiercaceresbordallo@MBP-de-Kceres.home>
2023-02-27 23:30:41 +0100

    Add Angular Universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'lab-dental-creative';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    const platform = isPlatformBrowser(this.platformId) ? 'browser' : 'server';
+    console.log("I'm in the", platform);
+  }
 }
